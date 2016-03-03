1e82d5f400732a42ca1404b3a697ac3360113c91
Stephen Brinkworth <xoman5144@gmail.com>
2016-03-03 16:35:06 -0700

    got player randomization working

--- src/App.js
+++ src/App.js
@@ -28,10 +28,13 @@ export default class App extends Component {
 
       playerCount: 5,
       setup: {resistance: 3, spies: 2},
-      teamSize: {1:2, 2:3, 3:2, 4:3, 5:3}
+      teamSize: {1:2, 2:3, 3:2, 4:3, 5:3},
+      players: []
     };
 
     this.selectNumberOfPlayers = this.selectNumberOfPlayers.bind(this);
+    this.startGame = this.startGame.bind(this);
+    this.shuffle = this.shuffle.bind(this);
 
   }
 
@@ -46,29 +49,92 @@ export default class App extends Component {
     });
   }
 
+  startGame(){
+    console.log('Starting Game');
+    var players = [];
+    var roles = [];
+    for (var i = 0; i < this.state.playerCount; i++) {
+      players.push({
+        name: 'Player ' + i + 1,
+        leader: false,
+        role: '',
+        cards: []
+      });
+    }
+
+    for (var j = 0; j < this.state.setup.resistance; j++) {
+      roles.push('Resistance');
+    }
+    for (var k = 0; k < this.state.setup.spies; k++) {
+      roles.push('Spy');
+    }
+
+    roles = this.shuffle(roles);
+    console.log(roles);
+
+    for (var l = 0; l < players.length; l++) {
+      players[l].role = roles[l];
+    }
+    console.log(players);
+
+    this.setState({
+      players: players
+    });
+
+    console.log(this.state.players);
+  }
+
+  shuffle(array) {
+    var m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+
+    return array;
+  }
+
+
   render() {
     return (
       <div>
 
         <h1>THE RESISTANCE</h1>
 
-        <select onChange={ (event) => this.selectNumberOfPlayers(event.target.value)}
-        >
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
+        <div className="sideBar">
+          <h3>Select number of players</h3>
+          <select onChange={ (event) => this.selectNumberOfPlayers(event.target.value)}>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
 
-        <h3>Player Count</h3>
-        Players: {this.state.playerCount}
-        <br/>
-        <h3>Setup</h3>
-        <Setup setup={this.state.setup}></Setup>
-        <h3>Team Sizes</h3>
-        <TeamSize teamSize={this.state.teamSize}></TeamSize>
+          <h3>Player Count</h3>
+          Players: {this.state.playerCount}
+          <br/>
+          <h3>Setup</h3>
+          <Setup setup={this.state.setup}></Setup>
+          <h3>Team Sizes</h3>
+          <TeamSize teamSize={this.state.teamSize}></TeamSize>
+
+          <button className="myButton" onClick={this.startGame}>Start Game</button>
+          {
+            this.state.players.map( (player) => {
+              return <li>{player.role}</li>
+            })
+          }
+        </div>
       </div>
     );
   }
